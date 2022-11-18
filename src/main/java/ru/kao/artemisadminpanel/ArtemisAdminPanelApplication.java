1128e057f7327fe4e6aeed6e6197b0e626731b33
taykita <nordac2001@gmai.com>
2022-11-18 23:00:24 +0300

    Initial commit

---
+++ src/main/java/ru/kao/artemisadminpanel/ArtemisAdminPanelApplication.java
@@ -0,0 +1,13 @@
+package ru.kao.artemisadminpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisAdminPanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemisAdminPanelApplication.class, args);
+	}
+
+}
